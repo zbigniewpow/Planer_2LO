a8694d794bad5e0e313564fdf6e457fe2e3604e1
zbigniewpow <72369335+zbigniewpow@users.noreply.github.com>
2026-09-02 12:20:16 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import PublicView from './pages/PublicView'
 import AdminView from './pages/AdminView'
+import PrintClasses from './pages/PrintClasses'
+import PrintTeachers from './pages/PrintTeachers'
 import ProtectedRoute from './components/ProtectedRoute'
 
 export default function App() {
@@ -9,6 +11,8 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/public" element={<PublicView />} />
+      <Route path="/drukuj/klasy" element={<PrintClasses />} />
+      <Route path="/drukuj/nauczyciele" element={<PrintTeachers />} />
       <Route
         path="/admin"
         element={
